aa83236494f61f800cf5e4c73a0fc02080f00f8c
jonginkim <jonginout@naver.com>
2018-03-27 16:08:36 +0900

    초기화

--- src/main/java/com/board/repository/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.board.repository.mapper;
-
-import com.board.repository.domain.User;
-
-public interface UserMapper {
-
-    public User getUser(String id) throws Exception;
-
-}
--- src/main/java/com/board/service/user/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.board.service.user;
-
-import com.board.repository.domain.User;
-
-public interface UserService {
-
-    public User loginUser(String id) throws Exception;
-
-}
