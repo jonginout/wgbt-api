50c399d3d718a5e7d33461844c3c3403700e8a2c
jonginkim <jonginout@naver.com>
2018-03-29 13:40:24 +0900

    init

---
+++ src/main/java/com/wgbt/wgbtapi/WgbtApiApplication.java
@@ -0,0 +1,12 @@
+package com.wgbt.wgbtapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WgbtApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WgbtApiApplication.class, args);
+	}
+}
